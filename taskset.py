93e4aafb739aab6026ab5662cbd2142f99ad68ff
kaisAlbi <kais.albichari@hotmail.com>
2017-12-03 13:00:35 +0100

    Correct P computation
    change max function to lcm function

--- taskset.py
+++ taskset.py
@@ -1,4 +1,5 @@
 from task import Task
+from functools import reduce
 
 class TaskSet:
     def __init__(self, task_set=[]):
@@ -12,10 +13,40 @@ class TaskSet:
 
     def findFeasibilityInterval(self):
         O_max = 0
-        P_max = 0
+        P = self.computeP()
         for task in self.getTaskSet():
             if(task.getOffset() > O_max):
                 O_max = task.getOffset()
-            if(task.getPeriod() > P_max):
-                P_max = task.getPeriod()
-        return [O_max, O_max + 2 * P_max]
+        return [O_max, O_max + 2 * P]
+
+    def GCD(self, a, b):
+        """
+        :param a:
+        :param b:
+        :return: greatest common divisor of a and b
+        """
+        if b == 0:
+            return a
+        else:
+            return self.GCD(b, a % b)
+
+    def lcm(self, a,b):
+        """
+        :param a:
+        :param b:
+        :return: least common multiple of a and b
+        """
+        return a*b//self.GCD(a,b)
+
+    def lcmArray(self,args):
+        """
+        :param args: list of int
+        :return: return the least common multiple of all values in args list
+        """
+        return reduce(self.lcm, args)
+
+    def computeP(self):
+        all_period = []
+        for task in self.getTaskSet():
+            all_period.append(task.getPeriod())
+        return self.lcmArray(all_period)
